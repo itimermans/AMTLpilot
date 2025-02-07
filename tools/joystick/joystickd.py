7165ac3356df0e10aa6d048d29061158734c188d
Comma Device <device@comma.ai>
2025-02-07 00:18:17 +0000

    sdvdv

--- tools/joystick/joystickd.py
+++ tools/joystick/joystickd.py
@@ -44,7 +44,7 @@ def joystickd_thread():
     # else:
     #   joystick_axes = [0.0, 0.0]
 
-    print(sm['carState'].steerFaultTemporary,"    ",CC.cruiseControl.resume)
+    print(sm['carState'].standstill,"    ",CC.cruiseControl.resume)
 
 
     if CC.longActive:
