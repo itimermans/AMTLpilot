e6e3cd11ccb3709a717b579c2519b2b765f0167f
Comma Device <device@comma.ai>
2025-05-12 08:35:39 -0500

    Save after Lightning test. Pre-Tesla Tests

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -863,13 +863,13 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
     ET.NO_ENTRY: NoEntryAlert("Sensor Data Invalid"),
     ET.SOFT_DISABLE: soft_disable_alert("Sensor Data Invalid"),
   },
-
+  ## AMT : Removing GPS reception warning
   EventName.noGps: {
-    ET.PERMANENT: Alert(
-      "Poor GPS reception",
-      "Ensure device has a clear view of the sky",
-      AlertStatus.normal, AlertSize.mid,
-      Priority.LOWER, VisualAlert.none, AudibleAlert.none, .2, creation_delay=600.)
+    # ET.PERMANENT: Alert(
+    #   "Poor GPS reception",
+    #   "Ensure device has a clear view of the sky",
+    #   AlertStatus.normal, AlertSize.mid,
+    #   Priority.LOWER, VisualAlert.none, AudibleAlert.none, .2, creation_delay=600.)
   },
 
   EventName.soundsUnavailable: {
