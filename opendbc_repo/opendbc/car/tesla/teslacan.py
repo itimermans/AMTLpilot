088a97ca387957aaaf1a5208da805c7ae1628c21
Comma Device <device@comma.ai>
2025-10-01 21:13:26 +0000

    extend dbc

--- opendbc_repo/opendbc/car/tesla/teslacan.py
+++ opendbc_repo/opendbc/car/tesla/teslacan.py
@@ -46,6 +46,24 @@ class TeslaCAN:
     values["DAS_controlChecksum"] = self.checksum(0x2b9, data[:7])
     return self.packer.make_can_msg("DAS_control", CANBUS.party, values)
 
+  # def create_1E5_message(self, counter):
+
+
+  #   values = {
+  #     "Debug_Nibble1A_Set_2": 2,
+  #     "Debug_Nibble3B_Set_2": 2,
+  #     "DAS_aebEvent": 0,
+  #     "DAS_jerkMin": CarControllerParams.JERK_LIMIT_MIN,
+  #     "DAS_jerkMax": CarControllerParams.JERK_LIMIT_MAX,
+  #     "DAS_accelMin": accel,
+  #     "DAS_accelMax": max(accel, 0),
+  #     "DAS_controlCounter": cntr,
+  #     "DAS_controlChecksum": 0,
+  #   }
+  #   data = self.packer.make_can_msg("debug_msg_party_1E5", CANBUS.party, values)[1]
+  #   values["DAS_controlChecksum"] = self.checksum(0x2b9, data[:7])
+  #   return self.packer.make_can_msg("debug_msg_party_1E5", CANBUS.party, values)
+
   def create_steering_allowed(self, counter):
     values = {
       "APS_eacAllow": 1,
