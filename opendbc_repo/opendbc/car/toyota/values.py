c9b536d5b1bc046a6248f118ed06c61de34bda59
Comma Device <device@comma.ai>
2026-06-11 18:55:49 +0000

    Comment out no dsu flag on camry id in values.py

--- opendbc_repo/opendbc/car/toyota/values.py
+++ opendbc_repo/opendbc/car/toyota/values.py
@@ -161,7 +161,8 @@ class CAR(Platforms):
     ],
     CarSpecs(mass=3400. * CV.LB_TO_KG, wheelbase=2.82448, steerRatio=13.7, tireStiffnessFactor=0.7933),
     dbc_dict('toyota_nodsu_pt_generated', 'toyota_adas'),
-    flags=ToyotaFlags.NO_DSU,
+    # Try this to force camry into exp mode
+    # flags=ToyotaFlags.NO_DSU,
   )
   TOYOTA_CAMRY_TSS2 = ToyotaTSS2PlatformConfig( # TSS 2.5
     [
